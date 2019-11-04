ba44a60c8ce3c67cd8c8f73109823ee6163fe0b6
Megane <fulconis.megane@gmail.com>
2019-11-04 16:01:33 +0100

    First commit and pushing to Heroku

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+
+require 'faker'
+
+
+Faker::Config.locale = :fr
+
+p "#####################################"
+p "DESTROY ALL"
+
+User.destroy_all
+p "USER DESTROY"
+
+5.times do |index|
+	  email = "email_#{index}@yopmail.com"
+    User.create!(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, description: Faker::Quote.famous_last_words,  encrypted_password: Faker::Creature::Animal.name, email: email)
+
+end
+p "database has now #{User.count} users"
