10a5ad4a93b4edb2d65bdfade617577ba717e9d8
Megane <fulconis.megane@gmail.com>
2019-11-04 16:58:41 +0100

    emails ok

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,9 @@
 class User < ApplicationRecord
 	has_many :events
 	has_many :attendances, through: :events
-	
+	after_create :welcome_send
+
+  def welcome_send
+    UserMailer.welcome_email(self).deliver_now
+  end
 end
