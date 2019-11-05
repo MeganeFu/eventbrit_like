de7bad39079ef93c67a059ba3eb8c5a1da1dec0a
Megane <fulconis.megane@gmail.com>
2019-11-05 22:13:37 +0100

    almost done for today

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,38 @@
 class EventsController < ApplicationController
+	before_action :authenticate_user
 	
 	def new
+		@event = Event.new
+	end
+	
+	private
+  
+    def authenticate_user
+        unless current_user
+            flash.now[:alert] = "Email ou mot de passe invalide"
+            redirect_to new_user_session_path
+        end
+    end
+	
+	
+	 def create
+		 
+		@event = Event.new
+		@event.title = params[:title]
+		@event.description = params[:description]
+		@event.location = params[:location]
+		@event.price = params[:price]
+		@event.duration = params[:duration]
+		@event.start_date = params[:start_date]
+		@event.user = current_user
+		@event.save
+		
+		if @event.save
+			flash[:success] = "Tu as ajouter ton super potin."
+			redirect_to welcome_index_path
+			
+		else render "new"
+
+	end
 	end
 end
